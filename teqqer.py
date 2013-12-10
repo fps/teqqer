c8697b47821740142ab3ac0326fbd1998707f850
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-12-10 22:04:08 +0100

    reformatting

--- teqqer.py
+++ teqqer.py
@@ -16,86 +16,24 @@ import teq
 
 help_text = u"""teqqer - a simple midi tracker
 
-F1 - Show this help
+Help:
 
-ESC - Enter global menu | Leave global menu | Leave this help | 
+  F1  - Show this help
+  ESC - Leave help and return to main screen
 
-TAB - Next sequence | Go to arrangement (if on last sequence)
+Navigating Menus:
 
-SHIFT-TAB - Previous sequence | Go to arrangement (if on first equence)
+  Menus are shown at the bottom of the screen. Press ESC to activate them.
 
-UP/DOWN - Move up/down in arrangement | move to next/previous tick in sequence
+  ESC - Show the global menu | Leave the global menu and return to main screen without taking an action
+  0-9 - Select a menu item | Select a submenu item (denoted by a trailing ">". E.g. "0 help>")
 
+Navigating the pattern and arrangement:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-LEFT/RIGHT - Move left/right in arrangement | Move from note to octave/from octave to note
+  TAB        - Next sequence | Go to arrangement (if on last sequence)
+  SHIFT-TAB  - Previous sequence | Go to arrangement (if on first equence)
+  UP/DOWN    - Move up/down in arrangement | move to next/previous tick in sequence
+  LEFT/RIGHT - Move left/right in arrangement | Move from note to octave/from octave to note
 """
 
 help_text_widget = urwid.Text(help_text)
