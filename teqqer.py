fdce29b5109346aeb38c4ae2b9ac0f04bc44b69d
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-12-28 04:00:52 +0100

    urwid ain't so hard

--- teqqer.py
+++ teqqer.py
@@ -20,15 +20,70 @@ import teq
 teq_engine = teq.teq()
 teq_engine.insert_midi_track("bd", 0)
 teq_engine.insert_midi_track("snare", 0)
-p = teq_engine.create_pattern(16)
+p = teq_engine.create_pattern(32)
 teq_engine.insert_pattern(0, p)
+
 
 class ui(urwid.Widget):
 	def __init__(self):
-		 self._sizing = frozenset(['box'])
-		 
+		urwid.Widget.__init__(self)
+		self._sizing = frozenset(['box'])
+		self.cursor_tick = 0
+		self.cursor_track = 0
+		self.cursor_column = 0
+		self.center_line_fraction = 0.3
+		self.highlighted_rows = 4
+	
+	def selectable(self):
+		return True
+	
+	def keypress(self, size, key):
+		pattern = teq_engine.get_pattern(0)
+		if key == "up":
+			self.cursor_tick = max(0, self.cursor_tick - 1)
+		if key == "down":
+			self.cursor_tick = min(pattern.length(), self.cursor_tick + 1)
+
+		self._invalidate()
+		
 	def render(self, size, focus):
-		pass
+		pattern = teq_engine.get_pattern(0)
+		
+		text = []
+		attr = []
+		
+		header = "ar tik"
+		for n in range(0, teq_engine.number_of_tracks()):
+			header = header + " " + '{0:<7.7}'.format(teq_engine.track_name(n))
+
+		text.append(header)
+		attr.append([('strong', len(header))])
+		
+		event_rows = size[1] - 2
+		
+		split = int(round(event_rows * self.center_line_fraction))
+		for n in range(0, event_rows):
+			displayed_tick = (self.cursor_tick + n) - split
+			if displayed_tick >= 0 and displayed_tick < pattern.length():
+				line = "   " + 	"%0.3x" % displayed_tick + " --- -- " * teq_engine.number_of_tracks()
+				line_attr = [(None, len(line))]
+				if displayed_tick % self.highlighted_rows == 0:
+					line_attr = [("weak", len(line))]
+				if displayed_tick == self.cursor_tick:
+					line_attr = [("strong", len(line))]
+				text.append(line)
+				attr.append(line_attr)
+			else:
+				text.append("")
+				attr.append([(None, len(""))])
+				
+		
+		menu = "esc: menu>  f1: help  space: play"
+		text.append(menu)
+		attr.append([("strong", len(menu))])
+		
+		t = urwid.TextCanvas(text, attr, maxcol = size[0]) 
+		return t
 
 #header_prefix_text = u"ar tk"
 #header_text = ""
@@ -46,5 +101,12 @@ class ui(urwid.Widget):
 
 the_ui = ui()
 
-loop = urwid.MainLoop(the_ui)
+palette = [
+	(None, "dark gray", "black"),
+	("weak", "light gray", "black"),
+	("strong", "light gray", "dark gray"),
+	("mega", "black", "white")
+]
+
+loop = urwid.MainLoop(the_ui, palette)
 loop.run()
