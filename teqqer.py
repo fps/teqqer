6d02d9470805c30eb344e01b1c5e9101f8a2861b
Florian Paul Schmidt <mista.tapas@gmx.net>
2014-01-02 17:47:06 +0100

    rendered my first midi event

--- teqqer.py
+++ teqqer.py
@@ -2,6 +2,7 @@ import sys
 import urwid
 import teq
 import pyteq
+import math
 
 usage_text = """
 Usage: teqqer [filename]
@@ -20,18 +21,29 @@ if (len(sys.argv) != 2):
 # These are the default options that can be overriden by a user's config
 # file and/or specific settings in a song [not implemented yet]
 default_options = {
-	"palette": [                 # The color palette (see urwid documentation)
+	# The color palette (see urwid documentation)
+	"palette": [                 
 		(None, "dark gray", "black"),
 		("weak", "light gray", "black"),
 		("strong", "light gray", "dark gray"),
 		("mega", "black", "white")
 	],
 	
-	"center_line_fraction": 0.3, # At what fraction of the screen to display the edit cursor
-	"highlighted_rows": 4,       # Highlight every highlight_row'th row
+	# At what fraction of the screen to display the edit cursor
+	"center_line_fraction": 0.3, 
 	
-	"cv_precision": 3,           # The number of digits used for displaying cv values
-	"control_precision": 3,      # The number of digits used for displaying control values
+	# Highlight every highlight_row'th row
+	"highlighted_rows": 4,
+	
+	# The number of digits used for displaying cv values (integer and fractional part)
+	"cv_fraction_precision": 3,
+	"cv_integer_precision": 1,
+	
+	# The number of digits used for displaying control values (integer and fractional part)
+	"control_fraction_precision": 3,
+	"control_integer_precision": 1,
+	
+	"column_separator": "  ",
 	
 	"follow_transport": True,
 	
@@ -177,13 +189,76 @@ class main(urwid.Widget):
 			ret = ret + self.render_key(item[0]) + ":" + item[1] + " "
 		return ret
 	
+	def midi_track_render_size(self):
+		return 6
+	
+	def control_track_render_size(self):
+		return self.options["control_integer_precision"] + 1 + self.options["control_fraction_precision"]
+	
+	def cv_track_render_size(self):
+		return self.options["control_integer_precision"] + 1 + self.options["control_fraction_precision"]
+		pass
+	
+	def render_midi_event(self, event):
+		if event.type == teq.midi_event_type.ON:
+			octave = math.floor(event.value1 / 12)
+			note = ["C ", "Db", "E ", "F ", "Gb", "G ", "Ab", "A ", "Bb", "B "][event.value1 % 12]
+			return note + "%0.1x" % octave + " " + "%0.2x" % event.value2
+		
+		return "--- --"
+	
+	def render_cv_event(self, event):
+		return "0" * self.options["cv_integer_precision"] + "." + "0" * self.options["cv_fraction_precision"]
+	
+	def render_control_event(self, event):
+		return "0" * self.options["control_integer_precision"] + "." + "0" * self.options["control_fraction_precision"]
+	
+	def render_track_name(self, name, maxlength):
+		if len(name) > maxlength:
+			return name[0:maxlength - 2] + ".."
+		
+		return name + " " * (maxlength - len(name))
+	
+	def render_pattern_line(self, pattern, tick):
+		column_separator = self.options["column_separator"]
+
+		line = "    " + "%0.3x" % tick
+		
+		for n in range(0, self.teq_engine.number_of_tracks()):
+			event = None
+			
+			if self.teq_engine.track_type(n) == teq.track_type.MIDI:
+				event = self.render_midi_event(pattern.get_midi_event(n, tick))
+			if self.teq_engine.track_type(n) == teq.track_type.CONTROL:
+				event = self.render_control_event(pattern.get_control_event(n, tick))
+			if self.teq_engine.track_type(n) == teq.track_type.CV:
+				event = self.render_cv_event(pattern.get_cv_event(n, tick))
+				
+			line = line + column_separator + event
+		
+		return line
+
+	
 	def render(self, size, focus):
 		text = []
 		attr = []
 		
-		header = "ar tik"
+		column_separator = self.options["column_separator"]
+		
+		header = "ar" + column_separator + "tik"
+		
 		for n in range(0, self.teq_engine.number_of_tracks()):
-			header = header + " " + '{0:<7.7}'.format(self.teq_engine.track_name(n))
+			track_name = self.teq_engine.track_name(n)
+			render_size = None
+			
+			if self.teq_engine.track_type(n) == teq.track_type.MIDI:
+				render_size = self.midi_track_render_size()
+			if self.teq_engine.track_type(n) == teq.track_type.CONTROL:
+				render_size = self.control_track_render_size()
+			if self.teq_engine.track_type(n) == teq.track_type.CV:
+				render_size = self.cv_track_render_size()
+				
+			header = header + column_separator + self.render_track_name(track_name, render_size)
 
 		header = self.fill_line(header, size[0])
 		
@@ -197,9 +272,11 @@ class main(urwid.Widget):
 		split = int(round(event_rows * self.options["center_line_fraction"]))
 		
 		for n in range(0, event_rows):
+			# Note: This might be negative
 			displayed_tick = (self.cursor_tick + n) - split
 			if displayed_tick >= 0 and displayed_tick < pattern.length():
-				line = "   " + 	"%0.3x" % displayed_tick + " --- -- " * self.teq_engine.number_of_tracks()
+				#line = "    " + "%0.3x" % displayed_tick + column_separator + "--- --"
+				line = self.render_pattern_line(pattern, displayed_tick)
 				line = self.fill_line(line, size[0])
 				line_attr = [(None, len(line))]
 				if displayed_tick % self.options["highlighted_rows"] == 0:
@@ -227,13 +304,16 @@ class main(urwid.Widget):
 teq_engine = teq.teq()
 teq_engine.insert_midi_track("bd", teq_engine.number_of_tracks())
 teq_engine.insert_midi_track("snare", teq_engine.number_of_tracks())
+teq_engine.insert_control_track("control", teq_engine.number_of_tracks())
 teq_engine.insert_midi_track("bd2", teq_engine.number_of_tracks())
 teq_engine.insert_midi_track("snare2", teq_engine.number_of_tracks())
+teq_engine.insert_cv_track("cv", teq_engine.number_of_tracks())
 teq_engine.insert_midi_track("bd3", teq_engine.number_of_tracks())
 teq_engine.insert_midi_track("snare3", teq_engine.number_of_tracks())
 teq_engine.insert_midi_track("bd4", teq_engine.number_of_tracks())
 teq_engine.insert_midi_track("snare4", teq_engine.number_of_tracks())
 p = teq_engine.create_pattern(64)
+p.set_midi_event(0, 0, teq.midi_event(teq.midi_event_type.ON, 64, 127))
 teq_engine.insert_pattern(0, p)
 
 
