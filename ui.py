6744cfc2ed0f7fe5fe4ec4414aadcf05e645b270
Florian Paul Schmidt <mista.tapas@gmx.net>
2014-01-22 23:11:19 +0100

    move stuff around

--- ui.py
+++ ui.py
@@ -6,6 +6,88 @@ import json
 import teq
 import pyteq
 
+class TextPopup(urwid.WidgetWrap):
+	def __init__(self, text):
+		text = urwid.Text(text)
+		help_list_box = urwid.ListBox(urwid.SimpleListWalker([text]))
+
+		self.__super.__init__(help_list_box)
+		
+		urwid.register_signal(TextPopup, 'close')
+
+	def keypress(self, size, key):
+		if key == 'esc':
+			self._emit('close')
+			return True
+		
+		self._w.keypress(size, key)
+
+class EditWithEnter(urwid.WidgetWrap):
+	def __init__(self, question):
+		self.history = []
+		self.edit = urwid.Edit(question)
+		help_list_box = urwid.ListBox(urwid.SimpleListWalker([self.edit]))
+		
+		self.__super.__init__(help_list_box)
+		urwid.register_signal(EditWithEnter, 'enter')
+
+	def keypress(self, size, key):
+		if key == 'enter':
+			self._emit('enter')
+			return True
+		
+		self._w.keypress(size, key)
+
+class PopUpLauncherThing(urwid.PopUpLauncher):
+	def __init__(self, original):
+		self.__super.__init__(original)
+		
+		self.the_original = original
+		
+		original.popup_launcher = self
+		
+		urwid.connect_signal(original, 'popup_about', lambda x: self.popup_about())
+		urwid.connect_signal(original, 'popup_license', lambda x: self.popup_license())
+		urwid.connect_signal(original, 'popup_help', lambda x: self.popup_help())
+
+	def popup_help(self):
+		self.popup_widget = TextPopup(("medium", the_help.get_help_text(self.the_original.options)))
+		urwid.connect_signal(self.popup_widget, 'close', lambda x: self.close_pop_up())
+		self.popup_parameters = {'left':0, 'top':0, 'overlay_width':200, 'overlay_height':200}
+		self.open_pop_up()
+
+	def popup_text(self, text):
+		self.popup_widget = TextPopup(("medium", "Press esc to leave this screen\n\n" + text))
+		urwid.connect_signal(self.popup_widget, 'close', lambda x: self.close_pop_up())
+		self.popup_parameters = {'left':0, 'top':0, 'overlay_width':200, 'overlay_height':200}
+		self.open_pop_up()
+
+	def popup_license(self):
+		self.popup_text(license.text)
+
+	def popup_about(self):
+		self.popup_text(about.text)
+		
+	def popup_line_entry(self, question, popup_parameters, callback):
+		self.callback = callback
+		self.popup_widget = EditWithEnter(("line-entry", question))
+		attr_map = urwid.AttrMap(self.popup_widget, "line-entry")
+		urwid.connect_signal(self.popup_widget, 'enter', lambda x: self.close_pop_up_line_entry())
+		self.popup_parameters = popup_parameters
+		self.open_pop_up()
+
+	def close_pop_up_line_entry(self):
+		self.callback(self.popup_widget.edit.edit_text)
+		#self.callback("foo")
+		self.close_pop_up()
+
+	def get_pop_up_parameters(self):
+		return self.popup_parameters
+	
+	def create_pop_up(self):
+		return self.popup_widget
+	
+
 class cursor_position():
 	def __init__(self, track = 0, pattern = 0, tick = 0):
 		self.pattern = pattern
