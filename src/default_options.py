69fdd2e8ec8cdeb9e25fa1ed9f9e0685625bdedc
Florian Paul Schmidt <mista.tapas@gmx.net>
2014-02-05 15:14:00 +0100

    and change the default edit step

--- src/default_options.py
+++ src/default_options.py
@@ -274,7 +274,7 @@ options = {
 	"cursor_wrap_mode": "song", 
 	
 	# How many ticks to skip down after editing an event
-	"edit_step": 4, 
+	"edit_step": 1, 
 	
 	# What key to press with the cursor keys to select stuff
 	"selection_modifier": "meta"
